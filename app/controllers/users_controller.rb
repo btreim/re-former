54fe6ace29f1f92322b228c6182d65b2d1e285ac
btreim <btreim@gmail.com>
2018-06-14 17:15:49 -0500

    implement edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params['id'])
+  end
+
   def update
-  	
+    @user = User.find(params[:user][:id])
+    @user.update(user_params)
+    redirect_to new_user_path
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:username,:email,:password)
+    params.require(:user).permit(:id,:username,:email,:password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resource :user, only: [:new, :create, :update]
+
+  get ':id/edit', to: 'users#edit'
 end
