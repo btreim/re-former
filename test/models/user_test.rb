10b36a12a39d6a1833a7676810d777bdd13087e5
btreim <btreim@gmail.com>
2018-06-14 13:58:38 -0500

    complete user validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,38 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "foo", email: "foo@bar.com",
+                     password: "foobaz1")
+  end
+
+  test "should be valid" do 
+  	assert @user.valid? 
+  end
+
+  test "username should be present" do
+  	@user.username = " "
+  	assert_not @user.valid? 
+  end
+
+  test "username should be unique" do
+  	dup_user = @user.dup
+  	@user.save
+  	assert_not dup_user.valid?
+  end
+
+  test "email should be present" do 
+  	@user.email = " "
+  	assert_not @user.valid?
+  end
+
+  test "password should be present" do
+  	@user.password = " " 
+  	assert_not @user.valid? 
+  end
+
+  test "password should not be under 6 characters" do
+  	@user.password = "a" * 5
+  	assert_not @user.valid? 
+  end
 end
